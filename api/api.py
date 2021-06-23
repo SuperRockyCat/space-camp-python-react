9583529821a6741b0cf8a0f9aae04c3a825b9d11
Rocky McGredy <rocky@Rockys-MacBook-Pro.local>
2021-06-23 14:15:59 -0700

    first commit

--- api/api.py
+++ api/api.py
@@ -1,8 +1,100 @@
 import time
+import ldclient
+import threading
+
+
+from ldclient.config import Config
 from flask import Flask
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123456789$"
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+app.host = 'localhost'
+
+###### LaunchDarkly SDK Key Goes HERE ######
+ld_sdk_key = ""
+ldclient.set_config(Config(ld_sdk_key))
+ld_client = ldclient.get()
+
+
+###### LaunchDarkly User Context ######
+user = {
+    "key" : "test",
+    "firstName" : "Space",
+    "lastName" : "Camp",
+    "email" : "spacecamp@launchdarkly.com"
+}
+
+
+###### LaunchDarkly Feature Flag Goes HERE ######
+def get_variation(user={"key":"test"}):
+    return ld_client.variation("my-feature-flag", user, "Not Set Up") # Add your flag here!
+
+
+
+###### Websockets for updates #######
+@socketio.on('connect')
+def connected():
+    send("%s-%s" % (user['key'], get_variation(user)), broadcast=True)
+
+@socketio.on('disconnect')
+def disconnected():
+    print('Disconnected')
+
+@socketio.on("message")
+def handleMessage(msg): 
+    print("handling message")
+    user.update({"key": msg})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###### Helper Code ######
+class FlagPoller(object):
+    def __init__(self, interval=1):
+        self.interval = interval
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            
+        thread.start()                                  
+
+    def run(self):
+        current_variation = get_variation(user)
+        current_user_key = user['key']
+        print("CURRENT VARIATION IS %s" % current_variation)
+
+        while True:
+            if current_variation != get_variation(user) or current_user_key != user['key']:
+                print("variation changed")
+                print("current_variation: %s" % current_variation)
+                print("get_variation(user): %s" % get_variation(user))
+                socketio.send("%s-%s" % (user['key'], get_variation(user)), broadcast=True)
+                current_variation = get_variation(user)
+                current_user_key = user['key']
+            else:
+                pass
+            time.sleep(self.interval)
+
+flag_poller = FlagPoller()
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True) 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import io from "socket.io-client";
+import toggle from './toggle.png';
 import './App.css';
 
-function App() {
+let endpoint = 'http://localhost:5000/';
+let socket = io.connect(endpoint);
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  socket.on("message", msg => {
+    if(typeof msg === "number" || typeof msg === "string"){
+      setMessages([...messages, msg])
+    }; 
+    
+    if(typeof msg === "boolean") {
+      setMessages([...messages, msg.toString()]);
+    };
+
+    if(typeof msg === "object") {
+      setMessages([...messages, JSON.stringify(msg)]);
+    };
+  });  
+
+  const onChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const onClick = () => {
+    socket.emit("message", message);
+    setMessage("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Welcome to Space Camp!
+        </h1>
+        <img src={toggle} className="App-logo" alt="logo" />
+      <div className="messages">
+        {messages.map(msg => (<p>User ID: <span className="userId">{msg.split("-")[0]}</span> <br /> Variation: <span className="variation">{msg.split("-")[1]}</span></p>))}
+      </div>
+      <p>
+        <input type="text" placeholder="Enter User ID" onChange={onChange} value={message} />
+        <input type="button" onClick={onClick} value="Send"/>
+      </p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
